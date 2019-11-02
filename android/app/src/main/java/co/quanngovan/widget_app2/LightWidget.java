3b378a52ee207303ebb47f06c819a45bb64c3f1a
van-quan.ngo <van-quan.ngo@ant-tech.eu>
2019-11-02 10:01:29 +0700

    second

---
+++ android/app/src/main/java/co/quanngovan/widget_app2/LightWidget.java
@@ -0,0 +1,53 @@
+package co.quanngovan.widget_app2;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class LightWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.light_widget);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://erenutku.com"));
+        // In widget we are not allowing to use intents as usually. We have to use PendingIntent instead of 'startActivity'
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        // Here the basic operations the remote view can do.
+        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
